2ca60408c26eb413ebb577b4d3001888c80a61a4
GandresCoello18 <goyeselcoca@gmail.com>
2021-05-26 00:55:51 -0500

    add card blog and info count instagram, location

--- src/components/card/CardVideo.tsx
+++ src/components/card/CardVideo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 
 export const CardVideo = () => (
@@ -5,38 +6,39 @@ export const CardVideo = () => (
     <div className="p-2 mb-5 grid grid-cols grid-rows grid-flow-row overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <div className="col-span-3 row-span-4 p-1 m-1">
         <a href="#">
-          <img
-            src="https://picsum.photos/640/400/?random"
-            alt="Placeholder"
-            className="rounded-t-xl object-cover h-48 w-full"
-          />
+          <video autoPlay loop muted className="rounded-t-xl object-cover h-48 w-full">
+            <source src="/images/localstorage.mp4" type="video/mp4" />
+          </video>
         </a>
-      </div>
-
-      <div className="col-span-3 row-span-1">
-        <div className="flex align-bottom flex-col leading-none p-2 md:p-4">
-          <div className="flex flex-row justify-between items-center">
-            <a className="flex items-center no-underline hover:underline text-black" href="#">
-              <img
-                alt="Placeholder"
-                className="block rounded-full"
-                src="https://picsum.photos/32/32/?random"
-              />
-              <span className="ml-2 text-sm"> John Doe </span>
-            </a>
-          </div>
-        </div>
       </div>
 
       <div className="col-span-3 row-span-1">
         <header className="flex items-center justify-between leading-tight p-2 md:p-4">
           <h1 className="text-lg">
             <a className="no-underline hover:underline text-black" href="#">
-              Title
+              Talleres y aplicacion de productos
             </a>
           </h1>
-          <p className="text-grey-darker text-sm">9 min ago</p>
         </header>
+      </div>
+
+      <div className="col-span-3 row-span-1">
+        <div className="flex align-bottom flex-col leading-none p-2 md:p-4">
+          <div className="flex flex-row justify-between items-center">
+            <a className="flex items-center no-underline hover:underline text-black" href="#">
+              <div className="flex -space-x-1 overflow-hidden">
+                {[0, 1, 2].map((item) => (
+                  <img
+                    key={item}
+                    alt="Placeholder"
+                    className="block rounded-full"
+                    src="https://picsum.photos/32/32/?random"
+                  />
+                ))}
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,85 @@
 import React from 'react';
 
+import { CardBlog } from '../components/card/CardBlog';
 import { CardVideo } from '../components/card/CardVideo';
 import { Main } from '../templates/Main';
 
 const Index = () => (
   <Main>
-    <div className="container m-auto grid sm:grid-cols-1 md:grid-cols-3 pt-6 gap-8">
+    <div className="container m-auto grid sm:grid-cols-1 lg:grid-cols-3 pt-6 gap-8 mt-7">
       {[1, 2, 3].map((item) => (
         <CardVideo key={item} />
       ))}
+    </div>
+
+    <div className="container m-auto grid sm:grid-cols-1 xl:grid-cols-3 pt-6 gap-8 mt-7 p-3">
+      {[1, 2, 3].map((item) => (
+        <CardBlog key={item} />
+      ))}
+    </div>
+
+    <div className="w-full m-auto bg-cici-100 grid grid-cols-1 md:grid-cols-2 pt-6 gap-8 mt-7 p-4">
+      <div className="p-3 mb-5 rounded-lg">
+        <div className="p-1 m-1">
+          <img
+            src="/images/local-cici.png"
+            alt="local fisico de cici beauty place"
+            className="rounded-t-xl bg-white object-cover p-3 w-auto float-right"
+          />
+        </div>
+      </div>
+
+      <div className="p-2 mb-5">
+        <div className="p-1 m-1 w-full md:w-96">
+          <h2 className="p-2 text-center text-white font-bold" style={{ fontSize: 30 }}>
+            ¿Que hacemos en Cici Beauty Place?
+          </h2>
+          <p className="p-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, suscipit? Aperiam illo,
+            vel ratione nobis provident ipsam magni debitis fuga porro repellat voluptas maxime,
+            quis tempora autem placeat fugit ullam.
+          </p>
+
+          <p className="p-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, suscipit? Aperiam illo,
+            vel ratione nobis provident ipsam magni debitis fuga porro repellat voluptas maxime,
+            quis tempora autem placeat fugit ullam.
+          </p>
+
+          <p className="p-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, suscipit? Aperiam illo.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div className="w-full m-auto bg-white grid grid-cols-1 md:grid-cols-2 pt-6 gap-8 mt-7 p-4">
+      <div className="p-2 mb-5">
+        <div className="p-1 m-1 w-full md:w-96 float-right">
+          <h2 className="p-2 text-center text-cici-300 font-bold" style={{ fontSize: 30 }}>
+            ¡Síguenos en Instagram!
+          </h2>
+          <p className="p-3">
+            En Instagram podemos tener un contacto permanente. En esta red social subo historias
+            diarias, publico mis pensamientos y recomendaciones a través de post y comparto videos
+            que pueden ser de mucha utilidad para ti. ¡Te veo ahí!
+          </p>
+        </div>
+      </div>
+
+      <div className="p-3 mb-5 rounded-lg">
+        <div className="p-1 m-1">
+          <img
+            src="/images/profile-inst.png"
+            alt="local fisico de cici beauty place"
+            className="rounded-t-xl bg-white object-cover p-3 w-auto float-left"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div className="w-full m-auto bg-cici-100 grid grid-cols-1 md:grid-cols-2 pt-6 gap-8 mt-7 p-4">
+      f
     </div>
   </Main>
 );
