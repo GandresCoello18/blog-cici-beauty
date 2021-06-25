467890aca298323c1eb5abc2131c5db919d08cc2
GandresCoello18 <goyeselcoca@gmail.com>
2021-06-25 12:33:04 -0500

    add menu notification user nav

---
+++ src/components/nav/MenuProfile.tsx
@@ -0,0 +1,87 @@
+import React, { Fragment } from 'react';
+
+import { Menu, Transition } from '@headlessui/react';
+import Link from 'next/link';
+
+export const MenuProfile = () => {
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ');
+}
+
+  return (
+    <Menu as="div" className="ml-3 relative z-10">
+      {({ open }: any) => (
+        <>
+          <div>
+            <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </Menu.Button>
+          </div>
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              <Menu.Item>
+                {({ active }: any) => (
+                  <Link href="/profile">
+                    <a
+                      href="/profile"
+                      className={classNames(
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      )}
+                    >
+                      Mi Perfil
+                    </a>
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }: any) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    )}
+                  >
+                    Ajustes
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }: any) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    )}
+                  >
+                    Salir
+                  </a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
+  )
+}
